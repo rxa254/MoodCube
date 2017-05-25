bd211e25cb1421e83abaf9a1af6e5e6db076cc25
Jameson Graef Rollins <jrollins@finestructure.net>
2017-05-25 00:54:20 -0700

    synapse zeromq processing framework
    Has three components communicating via zmq:
    
    source -+
    source -+-> mux --> plotter
    source -+
    
    Can be run via:
    
    $ LOG_LEVEL=info python3 -m synapse audio -p

---
+++ synapse/__main__.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import logging
+import argparse
+import importlib
+import collections
+import multiprocessing
+
+logging.basicConfig()
+
+from . import mux
+from . import sources
+from .sources import audio
+from . import plot
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--plot', action='store_true',
+                        help="plot data")
+    parser.add_argument('sources', metavar='SOUCE', nargs='+',
+                        help="source spec")
+
+    args = parser.parse_args()
+
+    procs = collections.OrderedDict()
+
+    logging.info("initializing mux...")
+    proc = multiprocessing.Process(
+        name='mux',
+        target=mux.element,
+        args=[args.sources],
+        daemon=True,
+        )
+    logging.info(proc)
+    procs['mux'] = proc
+
+    logging.info("initializing sources...")
+    for source_str in args.sources:
+        source_spec = source_str.split(':')
+        source = source_spec[0]
+        source_args = source_spec[1:]
+        logging.debug((source, source_args))
+    
+        func = eval('sources.{}.element'.format(source))
+        proc = multiprocessing.Process(
+            name=source_str,
+            target=func,
+            daemon=True,
+            )
+        logging.info(proc)
+        procs[source_str] = proc
+
+    if args.plot:
+        logging.info("initializing plotter...")
+        proc = multiprocessing.Process(
+            name='plot',
+            target=plot.plot,
+            daemon=True,
+            )
+        logging.info(proc)
+        procs['plot'] = proc
+
+    logging.info("starting processes...")
+    for source, proc in procs.items():
+        proc.start()
+        logging.info(proc)
+
+    try:
+        for source, proc in procs.items():
+            proc.join()
+    except KeyboardInterrupt:
+        raise SystemExit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ synapse/sources/audio.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import sys
+import zmq
+import json
+import pickle
+import signal
+import pyaudio
+import logging
+import numpy as np
+
+from .. import const
+
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+
+def element():
+    context = zmq.Context()
+
+    socket = context.socket(zmq.PUB)
+    socket.connect(const.MUX_SINK)
+    logging.info(socket)
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(
+        input=True,
+        format=pyaudio.paInt16,
+        channels=CHANNELS,
+        rate=RATE,
+        frames_per_buffer=CHUNK,
+        )
+
+    while True:
+        samples = stream.read(CHUNK)
+        dt = np.dtype(np.int16)
+        array = np.frombuffer(samples, dtype=dt)
+
+        source = 'audio'
+        msg = pickle.dumps({source: array})
+        socket.send_multipart((source.encode(), msg))
+
+##########
+
+def main():
+    channels = [int(c) for c in sys.argv[1:]]
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    element(channels)
+
+if __name__ == '__main__':
+    main()
