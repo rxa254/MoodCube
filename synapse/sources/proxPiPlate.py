640cbc5b1f09d2307d2991c3418d6803f3a79cb6
Rana Adhikari <rana@caltech.edu>
2017-06-08 21:35:33 -0700

    now has a main() so it can run as a module I guess?

--- synapse/sources/proxPiPlate.py
+++ synapse/sources/proxPiPlate.py
@@ -8,32 +8,39 @@ import numpy as np
 
 import DAQCplate as DAQC
 
-parser = argparse.ArgumentParser(description='Acquire Data from Pi Plate')
-parser.add_argument('-f','--fsample', dest='sample_frequency',
-                    metavar='fs', type=float,
-                    default = 1, help='Prox Sensor sample frequency [Hz]')
-args = parser.parse_args()
 
-fs = args.sample_frequency
-if __debug__:
-    print("Sample Frequency is " + str(fs) + " Hz")
-if fs < 1e-3:
-    parser.error("Error: sample rate must be > 1e-3 Hz")
+rangeLimit = 666.666
 
-rangeLimit = 300
-
-# this is the main loop
-j = 0
-while j < 1000:
-    x = DAQC.getRANGE(0, 0, 'c')  # get distance [cm]
-    if x < 0.1:
-        x = rangeLimit
-    if __debug__: print x
-    j += 1
-    time.sleep(1/fs)
+# this is the thing that get the data
+def element(fs):
+    while True:
+        x = DAQC.getRANGE(0, 0, 'c')  # get distance [cm]
+        if x < 0.1:
+            x = rangeLimit
+        if __debug__: print x
+        
+        time.sleep(1/fs)
 
 # this is the output when its done running
 # should also catch the CTRL-C
 
+# ===================================================
+def main():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    parser = argparse.ArgumentParser(description='get Data from Pi Plate')
+    parser.add_argument('-f','--fsample', dest='sample_frequency',
+                    metavar='fs', type=float,
+                    default = 1, help='Prox Sensor sample freq. [Hz]')
+    args = parser.parse_args()
 
+    fs = args.sample_frequency
+    if __debug__:
+        print("Sample Frequency is " + str(fs) + " Hz")
 
+    if fs < 1e-3:
+        parser.error("Error: sample rate must be > 1e-3 Hz")
+
+    element(fs)
+
+if __name__ == '__main__':
+    main()
