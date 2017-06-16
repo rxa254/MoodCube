c890062088045d0ff211e03c9cb4fe07478823c2
Rana Adhikari <rana@caltech.edu>
2017-06-16 13:43:59 -0700

    removed input_device setting; letting it use default

--- Audio/audio_blrms.py
+++ Audio/audio_blrms.py
@@ -58,7 +58,6 @@ stream = p.open(format=pyaudio.paInt16,
                 channels=1,
                 rate=RATE,
                 input=True,
-                input_device_index=0,
               frames_per_buffer = CHUNK)
 
 # define frequency bands for the BLRMS
