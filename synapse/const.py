7cdd54f411f9c0848ce1f2176b23bc59ca487162
Rana Adhikari <rana@caltech.edu>
2017-06-18 00:54:18 -0700

    update audio sample rate for Phoenix mic

--- synapse/const.py
+++ synapse/const.py
@@ -6,6 +6,8 @@ MUX_SINK = 'tcp://127.0.0.1:{}'.format(SYNAPSE_PORTS[0])
 MUX_SOURCE = 'tcp://127.0.0.1:{}'.format(SYNAPSE_PORTS[1])
 
 AUDIO_CHANNEL = 1
-AUDIO_RATE = 44100
+#AUDIO_RATE = 44100
+# for phoenix
+AUDIO_RATE = 16000
 
 OPC_ADDR = 'localhost:7890'
