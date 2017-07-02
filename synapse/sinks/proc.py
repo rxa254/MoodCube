ea382b5c23f8c3bdabfa213b2340d85c645c7276
Rana Adhikari <rana@caltech.edu>
2017-07-02 12:22:35 +0200

    made a for loop to implement arbitrary size networks instead of hard coding

--- synapse/sinks/proc.py
+++ synapse/sinks/proc.py
@@ -2,6 +2,60 @@ import numpy as np
 import collections
 import logging
 
+def act_fun(x):  
+    #y = 1/(1 + np.exp(-x))  # sigmoid function
+    y = np.tanh(x)  # tanh function
+    #y = x
+    #m = np.any([x <= 0])
+    #alpha = 1
+    #y[m] = alpha * (np.exp(x) - 1)
+    return y 
+
+def dact_fund(x): 
+    #y = x * (1-x) # derivative of sigmoid
+    y = 1 - x**2  # derivative of tanh
+    #y = 1
+    #m = np.any([x <= 0])
+    #alpha = 1
+    #y[m] = x + alpha
+    return y
+
+# forward propagate the input data through the matrices and thresholds
+def forward_prop(input_data, weights):
+    b = input_data
+    for layer_name in sorted(weights.keys()):
+        b = np.dot(weights[layer_name].T, b)
+        b = act_fun(b)
+    return b
+
+def compute_gradient(gradient_log_p, hidden_layer_values,
+                         input_data, weights):
+    """ See here: http://neuralnetworksanddeeplearning.com/chap2.html"""
+    delta_L = gradient_log_p
+    dC_dw2 = np.dot(hidden_layer_values.T, delta_L).ravel()
+    delta_l2 = np.outer(delta_L, weights['2'])
+    delta_l2 = relu(delta_l2)
+    dC_dw1 = np.dot(delta_l2.T, observation_values)
+    return {
+        '1': dC_dw1,
+        '2': dC_dw2
+    }
+
+# use the computed gradients to update the weights
+def update_weights(weights, expectation_g_squared, g_dict,
+                       decay_rate, learning_rate):
+    epsilon = 1e-5
+    for layer_name in sorted(weights.keys()):
+        g = g_dict[layer_name]
+        expectation_g_squared[layer_name] = decay_rate * \
+          expectation_g_squared[layer_name] + (1 - decay_rate) * g**2
+        weights[layer_name] += (learning_rate * g) /  \
+          (np.sqrt(expectation_g_squared[layer_name] + epsilon))
+        # reset batch gradient buffer
+        g_dict[layer_name] = np.zeros_like(weights[layer_name])
+
+    return weights, expectation_g_squared, g_dict
+
 
 class DataBuffer(object):
     def __init__(self, sources, samples):
@@ -50,82 +104,49 @@ class ProcessData(object):
         # input data ring buffer
         self.db = DataBuffer(sources, samples)
 
-        self.inpLayer   = None
-        self.twoLayer   = None
-        self.threeLayer = None
-        self.fourLayer  = None
-        self.outLayer   = None
+        self.weights = {}
 
+            
     def __call__(self, packet):
         self.db.append(packet)
         data = self.db.get()
         logging.debug(len(data))
         #print(data)
 
-        print("audio_blrms = " + str(data[0:8]))
-        print("proximity   = " + str(data[8:12]))
-        print("datetime    = " + str(data[12:16]))
-        l1_error      = 0.01 * np.sum(data[0:8]  - audio_target)
-        l1_prox_error = 0.01 * np.sum(data[8:12] - prox_target)
+
+        print("audio_blrms = " + np.array_str(data[0:8], precision=3))
+        print("proximity   = " + np.array_str(data[8:12], precision=3))
+        print("datetime    = " + np.array_str(data[12:16], precision=3))
+        l1_error      = 0.1 * np.sum(data[0:8]  - audio_target)
+        l1_prox_error = 0.1 * np.sum(data[8:12] - prox_target)
 
         nL = len(data)
-        
-        if self.inpLayer is None:
-            self.inpLayer   = 0.1 * np.random.randn(nL, nL)
-            self.twoLayer   = 0.1 * np.random.randn(nL, nL)
-            self.threeLayer = 0.1 * np.random.randn(nL, Nsensors)
-            self.fourLayer  = 0.1 * np.random.randn(numLEDs, Nsensors)
-            self.outLayer   = 0.1 * np.random.randn(3*numLEDs, numLEDs)
-            
 
-        # print((data.shape, self.inpLayer.shape))
-        a  = np.dot(self.inpLayer, data)
-        #print(a)
-        #a /= np.amax(a)
+        numNeurons = [nL, nL, 3*numLEDs]
 
-        # Exponential Linear Units https://arxiv.org/abs/1511.07289
-        #alpha = 1
-        #a = alpha * (np.exp(a) - 1)
-        a  = np.tanh(a)/2 + 1          # goes from 0->1 as x -> minux to plus inf
-        l1_delta = (l1_error + 1 * l1_prox_error) * a
-        self.inpLayer += np.outer(l1_delta, data)
-        print "Output of input layer = " + str(a[0:5])
+        # build the initial weight matrices
+        if len(self.weights) == 0:
+            for j in range(len(numNeurons)-1):
+                w = {str(j):np.random.randn(numNeurons[j], numNeurons[j+1])}
+                self.weights.update(w)
         
-        #a  = np.tanh(a)/2 + 1
-        #print(a)
-        #print((a.shape, self.twoLayer.shape))
-        b = np.dot(self.twoLayer, a)
-        #a = alpha * (np.exp(a) - 1)
-        b  = np.tanh(b)/2 + 1
-        l1_delta = (l1_error + 1 * l1_prox_error) * b
-        self.twoLayer += np.outer(l1_delta, a)
-        print "Output of second layer = " + str(b[0:5])
+        # To be used with rmsprop algorithm
+        # (http://sebastianruder.com/optimizing-gradient-descent/index.html#rmsprop)
+        expectation_g_squared = {}
+        g_dict = {}
+        for layer_name in self.weights.keys():
+            expectation_g_squared[layer_name] = np.zeros_like(self.weights[layer_name])
+            g_dict[layer_name] = np.zeros_like(self.weights[layer_name])
 
-        a = b
-        b = np.dot(self.threeLayer, a)
-        b = np.tanh(b)/2 + 1
-        l1_delta = (l1_error + 1 * l1_prox_error) * b
-        self.threeLayer += np.outer(l1_delta, a)
-        #print b
+        # go forward through the network
+        output = forward_prop(data, self.weights)
         
-        #print a.shape
-        a = b
-        b = np.dot(self.fourLayer, a)
-        b = np.tanh(b)/2 + 1
-        l1_delta = (l1_error + 1 * l1_prox_error) * b
-        self.fourLayer += np.outer(l1_delta, a)
-        #print b
-        
-        zz = np.dot(self.outLayer, b)
-        zz = np.tanh(zz)/2 + 1
-        #print zz
-        
-        
-        bias_noise = 55 * np.random.randn(numLEDs, 3)
-        zz = 100*zz    # scale to output range of 100 (lowered from 255 for power)
+        bias_noise = np.random.randn(numLEDs, 3)
+        # scale to output range of 100 (lowered from 255 for power)
+        output = 100 * (output + 1) / 2
 
-        print zz[7:15]
-        out = zz.reshape(numLEDs, 3) + (0.0) * bias_noise
+        print("out = " + np.array_str(output[7:15], precision=2))
+        output = output.reshape(numLEDs, 3) + (0) * bias_noise
         #out = bias_noise
         # return 512 x 3 matrix for LEDs
-        return out
+        return output
